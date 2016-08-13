b42fdfb267f1572396b1d73b9f924aedada608f7
Timothy Voice <timsethvoice@gmail.com>
2016-08-13 11:22:03 -0400

    added server notes

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ import { apolloServer } from 'apollo-server';
 import Schema from './schema.js';
 import Resolvers from './resolvers.js';
 
+// Create an express server called app
 const app = express();
+
+// We're going to need to tell our server wich port to use
+// We're going to deploy to Heroku later, and Heroku,
+// dynamically assigns the port to our app so we need
+// to read the port from an env variable.
+// In local development we want to use a consistant port
+// In this case, port 8000
 const GRAPHQL_PORT = (process.env.PORT || 8000);
 
+// We want to expose a /graphql endpoint and configure apolloServer
+// Note: the schema and resolvers fields are required and your
+// graphql server wont work without them
 app.use('/graphql', apolloServer({
   graphiql: true,
   pretty: true,
@@ -13,6 +24,9 @@ app.use('/graphql', apolloServer({
   resolvers: Resolvers,
 }));
 
+// Tell your server to listen for requests on your
+// GRAPHQL_PORT and log some message to the console
+// so we know the server is up and running
 app.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
 ));
